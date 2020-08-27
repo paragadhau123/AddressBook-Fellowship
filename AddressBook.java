07f3ef3a5b3215375fecf438f1ce1b23284f766b
parag <adhauparag64@gmail.com>
2020-08-27 23:22:42 +0530

    Added :Add new person functionality to address book

--- AddressBook.java
+++ AddressBook.java
@@ -1,21 +1,7 @@
 public class AddressBook {
     public static void main(String[] args) {
         System.out.println("Welcome To Address Book");
-        String fname,lname,address,city,state,zip,phone;
-        System.out.println("Enter First Name");
-        fname=InputUtil.getStringValue();
-        System.out.println("Enter Last Name");
-        lname=InputUtil.getStringValue();
-        System.out.println("Enter Address");
-        address=InputUtil.getStringValue();
-        System.out.println("Enter City");
-        city=InputUtil.getStringValue();
-        System.out.println("Enter State");
-        state=InputUtil.getStringValue();
-        System.out.println("Enter Zip");
-        zip=InputUtil.getStringValue();
-        System.out.println("Enter phone number");
-        phone=InputUtil.getStringValue();
-
+        final Helper help = new Helper();
+        help.addRecord();
     }
 }
---
+++ Helper.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Helper {
+    //	ADD METHOD
+    public void addRecord()
+    {
+        final String fname, lname, address, city, state, phone,zip;
+
+        System.out.print("Enter First Name : ");
+        fname = InputUtil.getStringValue();
+        System.out.print("Enter Last Name : ");
+        lname = InputUtil.getStringValue();
+        System.out.print("Enter Phone Number : ");
+        phone = InputUtil.getStringValue();
+        System.out.print("Enter Address : ");
+        address = InputUtil.getStringValue();
+        System.out.print("Enter city : ");
+        city = InputUtil.getStringValue();
+        System.out.print("Enter zip : ");
+        zip = InputUtil.getStringValue();
+        System.out.print("Enter state : ");
+        state = InputUtil.getStringValue();
+
+        List<Person> person = Arrays.asList(
+                new Person(fname,lname,address,city,state,phone,zip)
+        );
+
+    } // END of addRecord()
+}
